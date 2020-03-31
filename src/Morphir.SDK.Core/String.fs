5be81c296f16c90a0c96f65fe2537f4ce9d9922f
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2020-03-31 17:26:23 -0400

    Add the cons and join functions for String - #3

--- src/Morphir.SDK.Core/String.fs
+++ src/Morphir.SDK.Core/String.fs
@@ -1,7 +1,6 @@
 module Morphir.SDK.String
 
 open Morphir.SDK
-open Morphir.SDK
 
 let inline isEmpty string =
     System.String.IsNullOrEmpty(string)
@@ -16,6 +15,9 @@ let inline toLower (str:string) =
 let inline toUpper (str:string) =
     str.ToUpperInvariant()
 
+let cons (ch:char) (string:string) =
+    sprintf "%c%s" ch string
+
 let uncons (string:string) =
     match string  with
     | null -> Maybe.Nothing
@@ -23,3 +25,5 @@ let uncons (string:string) =
     | str when (str.Length = 1) -> Maybe.Just  (str.[0], System.String.Empty)
     | str -> Maybe.Just (str.[0], str.[1..])
 
+let join (sep:string) (chunks:Morphir.SDK.List.List<string>) =
+    System.String.Join(sep, chunks)
