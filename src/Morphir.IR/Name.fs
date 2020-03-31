418db5ef222c552d13299696d91fa3cef17dbbc9
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2020-03-31 16:35:38 -0400

    Start the creation of the Name type and supporting SDK functionality in F# (#1)
    * Get the basics of creating a name working
    
    * Provide some basic String implementations

--- src/Morphir.IR/Name.fs
+++ src/Morphir.IR/Name.fs
@@ -2,13 +2,20 @@ module Morphir.IR.Name
 
 open Morphir.SDK
 
-open Morphir.SDK.Regex
-type Name = string list
+type Name = Morphir.SDK.List.List<string>
+
+let inline fromList (words:string list):Name =
+    words
 
 let fromString (string: string): Name =
     let wordPattern =
         Regex.fromString "([a-zA-Z][a-z]*|[0-9]+)"
         |> Maybe.withDefault Regex.never
 
-    failwith "Not Implemented"
-let inline fromList (words: string list): Name = words
+    Regex.find wordPattern string
+        |> List.map (fun me -> me.Match)
+        |> List.map String.toLower
+        |> fromList
+
+let toList  (name:Name) : List<string> =
+    name
