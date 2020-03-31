5ada412fc3915148acb58155196e8bb74cd0f0fd
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2020-03-31 19:13:55 -0400

    Provide support for the Name Type (#6)
    * Add the cons and join functions for String - #3
    
    * Provide the Name type along with supporting SDK functionality

---
+++ src/Morphir.SDK.Core/Char.fs
@@ -0,0 +1,6 @@
+module Morphir.SDK.Char
+
+type Char = char
+
+let inline toUpper (ch:Char) : Char =
+    Char.ToUpperInvariant(ch)
--- tests/Morphir.IR.Tests/NameTests.fs
+++ tests/Morphir.IR.Tests/NameTests.fs
@@ -1,22 +1,67 @@
 module Morphir.IR.NameTests
 
-open System
-open Expecto
+open Morphir.SDK.Testing
 open Morphir.IR
-
-let expectEqual expected actual =
-    Expect.equal actual expected (sprintf "Expected %A to equal %A" actual expected)
 
 [<Tests>]
 let tests =
-    testList "NameTests"
-        [ testList "fromString"
-              [ let assert' inString outList =
-                  testCase ("From string" + inString)
-                  <| fun _ -> Name.fromString inString |> expectEqual (Name.fromList outList)
+    let fromString =
+        describe "fromString" [
+            let assert' inString outList =
+              testCase ("From string" + inString)
+              <| fun _ -> Name.fromString inString |> Expect.equal (Name.fromList outList)
 
-                assert' "fooBar_baz 123" [ "foo"; "bar"; "baz"; "123" ]
-                assert' "valueInUSD" [ "value"; "in"; "u"; "s"; "d" ]
-                assert' "ValueInUSD" [ "value"; "in"; "u"; "s"; "d" ]
-                assert' "value_in_USD" [ "value"; "in"; "u"; "s"; "d" ]
-                assert' "_-% " [] ] ]
+            assert' "fooBar_baz 123" [ "foo"; "bar"; "baz"; "123" ]
+            assert' "valueInUSD" [ "value"; "in"; "u"; "s"; "d" ]
+            assert' "ValueInUSD" [ "value"; "in"; "u"; "s"; "d" ]
+            assert' "value_in_USD" [ "value"; "in"; "u"; "s"; "d" ]
+            assert' "_-% " []
+        ]
+
+    let toTitleCaseTests =
+        describe "toTitleCase" [
+            let assert' inList outString =
+                testCase (sprintf "Title case %s" outString)
+                <| fun _ -> Name.fromList inList |> Name.toTitleCase |> Expect.equal outString
+
+            assert' [ "foo"; "bar"; "baz"; "123" ] "FooBarBaz123"
+            assert' [ "value"; "in"; "u"; "s"; "d" ] "ValueInUSD"
+        ]
+
+    let toCamelCaseTests =
+        describe "toCamelCase" [
+            let assert' inList outString =
+                testCase (sprintf "Title case %s" outString)
+                <| fun _ -> Name.fromList inList |> Name.toCamelCase |> Expect.equal outString
+
+            assert' [ "foo"; "bar"; "baz"; "123" ] "fooBarBaz123"
+            assert' [ "value"; "in"; "u"; "s"; "d" ] "valueInUSD"
+        ]
+
+    let toSnakeCaseTests =
+        describe "toSnakeCase" [
+            let assert' inList outString =
+                testCase (sprintf "Title case %s" outString)
+                <| fun _ -> Name.fromList inList |> Name.toSnakeCase |> Expect.equal outString
+
+            assert' [ "foo"; "bar"; "baz"; "123" ] "foo_bar_baz_123"
+            assert' [ "value"; "in"; "u"; "s"; "d" ] "value_in_USD"
+        ]
+
+    let toHumanWordsTests =
+        describe "toHumanWords" [
+            let assert' inList outList =
+                testCase (sprintf "Title case %A" outList)
+                <| fun _ -> Name.fromList inList |> Name.toHumanWords |> Expect.equal outList
+
+            assert' [ "foo"; "bar"; "baz"; "123" ] ["foo"; "bar"; "baz"; "123"]
+            assert' [ "value"; "in"; "u"; "s"; "d" ] ["value"; "in"; "USD"]
+        ]
+
+    describe "NameTests" [
+        fromString
+        toTitleCaseTests
+        toCamelCaseTests
+        toSnakeCaseTests
+        toHumanWordsTests
+    ]
