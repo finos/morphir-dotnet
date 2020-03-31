25a3e5037c8592c361a148500652f2d049d72fff
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2020-03-31 19:13:55 -0400

    Provide support for the Name Type (#6)
    * Add the cons and join functions for String - #3
    
    * Provide the Name type along with supporting SDK functionality

--- src/Morphir.IR/Name.fs
+++ src/Morphir.IR/Name.fs
@@ -1,5 +1,8 @@
 module Morphir.IR.Name
 
+open Morphir.SDK.Maybe
+open Morphir.SDK.String
+open Morphir.SDK
 open Morphir.SDK
 
 type Name = Morphir.SDK.List.List<string>
@@ -17,5 +20,55 @@ let fromString (string: string): Name =
         |> List.map String.toLower
         |> fromList
 
-let toList  (name:Name) : List<string> =
+let inline toList  (name:Name) : List<string> =
     name
+
+let capitalize string : string =
+    match String.uncons string with
+    | Just (headChar, tailString) ->
+        String.cons (Char.toUpper headChar) tailString
+    | Nothing -> string
+
+let toTitleCase name =
+    name
+    |> toList
+    |> List.map capitalize
+    |> String.join ""
+
+let toCamelCase (name:Name) =
+    match name |> toList with
+    | [] -> System.String.Empty
+    | head :: tail ->
+        tail
+        |> List.map capitalize
+        |> List.cons head
+        |> String.join ""
+
+let toHumanWords name : List<string> =
+    let words = toList name
+
+    let join abbrev =
+        abbrev
+        |> String.join ""
+        |> String.toUpper
+
+    let rec process' prefix abbrev suffix =
+        match suffix with
+        | [] ->
+            if (List.isEmpty abbrev) then
+                prefix
+            else
+                List.append prefix [join abbrev]
+        | first :: rest ->
+            if (String.length first = 1) then
+                process' prefix (List.append abbrev [first]) rest
+            else
+                match abbrev with
+                    | [] -> process' (List.append prefix [first]) [] rest
+                    | _ -> process' (List.append prefix [join abbrev; first]) [] rest
+    process' [] [] words
+
+let toSnakeCase name =
+    name
+    |> toHumanWords
+    |> String.join "_"
--- src/Morphir.SDK.Core/List.fs
+++ src/Morphir.SDK.Core/List.fs
@@ -2,5 +2,18 @@ module Morphir.SDK.List
 
 type List<'t> = 't list
 
+let inline singleton a =
+    [a]
+
+let inline cons a lst =
+    a :: lst
+
 let inline map mapping list =
     Microsoft.FSharp.Collections.List.map mapping list
+
+let isEmpty = function
+    | [] -> true
+    | _ -> false
+
+let inline append list1 list2 =
+    FSharp.Collections.List.append list1 list2
