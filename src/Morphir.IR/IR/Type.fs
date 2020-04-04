218280d2ac8e3b111b36ab3be18b331a12753275
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2020-04-04 14:42:44 -0400

    Implementing the IR #16

--- src/Morphir.IR/IR/Type.fs
+++ src/Morphir.IR/IR/Type.fs
@@ -1,10 +1,12 @@
 module Morphir.IR.Type
 
+open Morphir.Pattern
 open Morphir.IR.AccessControlled
 open Morphir.IR.Name
 open Morphir.IR.FQName
 open Morphir.SDK.Maybe
 open Morphir.IR
+open Morphir.SDK
 
 type Type<'A>
     = Variable of Attributes:'A * Name:Name
@@ -31,7 +33,7 @@ and Field<'A> =
     { Name: Name
       Type: Type<'A> }
 and Constructor<'A>
-    = Constructor of Name * (Name * Type<'A>) list
+    = Constructor of Name:Name * Types:(Name * Type<'A>) list
 and Constructors<'A>
     = Constructor<'A> list
 and Specification<'A>
@@ -41,6 +43,14 @@ and Specification<'A>
 and Definition<'A>
     = TypeAliasDefinition of TypeParams:Name list * TypeExpr:Type<'A>
     | CustomTypeDefinition of TypeParams:Name list * Constructors:AccessControlled<Constructors<'A>>
+
+type Field<'A> with
+    member this.MapName f =
+        {this with Name = f(this.Name) }
+
+    member this.MapType f =
+        {Name=this.Name; Type = f(this.Type) }
+
 
 let definitionToSpecification (def:Definition<'A>) : Specification<'A> =
     match def with
@@ -52,6 +62,7 @@ let definitionToSpecification (def:Definition<'A>) : Specification<'A> =
                 CustomTypeSpecification(p, ctors)
         | Nothing ->
                 OpaqueTypeSpecification p
+
 
 let variable attributes name =
     Variable(attributes, name)
@@ -89,5 +100,67 @@ let opaqueTypeSpecification typeParams =
 let customTypeSpecification typeParams ctors =
     CustomTypeSpecification(typeParams, ctors)
 
+let field name fieldType =
+    { Name = name; Type = fieldType}
+
+let mapFieldName f (field:Field<'A>) =
+    field.MapName f
+
+let mapFieldType f (field:Field<'A>) : Field<'B> =
+    field.MapType f
+
+let matchField (matchFieldName:Pattern<Name, 'A>) (matchFieldType:Pattern<Type<'A>,'B>) field =
+    Maybe.map2 Tuple.pair
+        (matchFieldName field.Name)
+        (matchFieldType field.Type)
+
+let rec mapTypeAttributes f = function
+    | Variable (a, name) ->
+        Variable ((f a), name)
+    | Reference (a, fQName, argTypes) ->
+        let newArgTypes =
+            argTypes
+            |> List.map (mapTypeAttributes f)
+        Reference ((f a), fQName, newArgTypes)
+
+    | Tuple (a, elemTypes) ->
+        Tuple ((f a), (elemTypes |> List.map (mapTypeAttributes f)))
+
+    | Record (a, fields) ->
+        Record ((f a), (fields |> List.map (mapFieldType (mapTypeAttributes f))))
+
+    | ExtensibleRecord (a, name, fields) ->
+        ExtensibleRecord ((f a), name, (fields |> List.map (mapFieldType (mapTypeAttributes f))))
+
+    | Function (a, argType, returnType) ->
+        Function ((f a), (argType |> mapTypeAttributes f), (returnType |> mapTypeAttributes f))
+    | Unit a -> Unit (f a)
+
 let typeAttributes (typeExpr:Type<'Attributes>) : 'Attributes =
     typeExpr.Attributes
+
+let eraseAttributes (typeDef:Definition<'A>) : Definition<Unit> =
+    let mkUnit a =  ()
+
+    match typeDef with
+    | TypeAliasDefinition(typeVars, tpe) ->
+        TypeAliasDefinition(typeVars, (mapTypeAttributes mkUnit tpe))
+    | CustomTypeDefinition(typeVars, acsCtrlConstructors) ->
+        let eraseCtor (ctor:Constructor<'A>) =
+            let (Constructor (name, types)) = ctor
+            let extraErasedTypes =
+                types
+                |> List.map (fun ( n, t ) -> ( n, t |> mapTypeAttributes mkUnit ))
+
+            Constructor (name, extraErasedTypes)
+
+        let eraseAccessControlledCtors acsCtrlCtors =
+                AccessControlled.map
+                    (fun ctors -> ctors |> List.map eraseCtor)
+                    acsCtrlCtors
+
+        CustomTypeDefinition (typeVars, (eraseAccessControlledCtors acsCtrlConstructors))
+
+type Definition<'A> with
+    member this.EraseAttributes () =
+        eraseAttributes this
