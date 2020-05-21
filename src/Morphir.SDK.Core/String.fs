c3249617f7e54b2705ab4bd5179bcf9295a91a60
Michelle <michelchan421@gmail.com>
2020-05-21 19:34:27 -0400

    adding more string methods + test cases #3

--- src/Morphir.SDK.Core/String.fs
+++ src/Morphir.SDK.Core/String.fs
@@ -9,7 +9,10 @@ let length = function
     | null -> 0
     | (str:string) -> str.Length
 
-let repeat (str:string) (x:int) =
+let reverse (str:string) =
+    str |> Seq.rev |> System.String.Concat
+
+let repeat (x:int) (str:string) =
     String.replicate x str
 
 let replace (before:string) (after:string) (str:string) =
@@ -19,7 +22,7 @@ let append (str1:string) (str2:string) =
     str1 + str2
 
 let split (sep:string) (str:string) =
-    str.Split sep
+    str.Split([|sep|], System.StringSplitOptions.None) |> Array.toList
 
 let join (sep:string) (chunks:Morphir.SDK.List.List<string>) =
     System.String.Join(sep, chunks)
@@ -28,13 +31,15 @@ let concat (strings: string list) =
     join "" strings
 
 let words (str:string) =
-    str.Split "\s"
+    split " " str
 
 let lines (str:string) =
-    str.Split "\n"
+   split "\n" str
 
 let slice (startIndex:int) (endIndex: int) (str:string) =
-    str.[startIndex..endIndex]
+    let start = if startIndex >= 0 then startIndex else str.Length + startIndex
+    let last = if endIndex >= 0 then endIndex-1 else (str.Length + endIndex - 1)
+    str.[start..last]
 
 let left (n:int) (str:string) =
     if n < 1 then ""
@@ -61,22 +66,35 @@ let startsWith (substring:string) (str:string) =
 let endsWith (substring:string) (str:string) =
     str.EndsWith substring
 
-let rec indexesHelp (substring:string) (str:string) (result: int list) =
+let rec indexesHelp (substring:string) (str:string) (curr:int) (result: int list) =
     let idx = str.IndexOf substring
     if (idx = -1) then result
-    else indexesHelp substring (right idx str) (result @ [idx])
+    else indexesHelp substring (slice (idx+1) str.Length str) (curr+idx+1) (result @ [curr+idx])
 
 let indexes (substring:string) (str:string) =
-    indexesHelp substring str []
+    indexesHelp substring str 0 []
 
 let indices (substring:string) (str:string) =
     indexes substring str
 
-let inline toLower (str:string) =
-    str.ToLowerInvariant()
+let toInt (str: string) =
+    match System.Int32.TryParse str with
+    | (true, result) -> Some(result)
+    | (false, _) -> None
 
-let inline toUpper (str:string) =
-    str.ToUpperInvariant()
+let fromInt (n: int) =
+    string n
+
+let toFloat (str: string) =
+    match System.Double.TryParse str with
+    | (true, result) -> Some(result)
+    | (false, _) -> None
+
+let fromFloat (n: float) =
+    string n
+
+let fromChar (ch: char) =
+    string ch
 
 let cons (ch:char) (string:string) =
     sprintf "%c%s" ch string
@@ -87,3 +105,50 @@ let uncons (string:string) =
     | "" -> Maybe.Nothing
     | str when (str.Length = 1) -> Maybe.Just  (str.[0], System.String.Empty)
     | str -> Maybe.Just (str.[0], str.[1..])
+
+let toList (str:string) =
+    Seq.toList str
+
+let fromList (chs:char list) =
+    System.String.Concat(Array.ofList(chs))
+
+let inline toUpper (str:string) =
+    str.ToUpperInvariant()
+
+let inline toLower (str:string) =
+    str.ToLowerInvariant()
+
+let pad (n:int) (ch:char) (str:string) =
+  let half:float = float(n - length str) / 2.0
+  repeat (ceil half |> int) (fromChar ch) + str + repeat (floor half |> int) (fromChar ch)
+
+let padLeft (n:int) (ch:char) (str:string) =
+    repeat (n - length str) (fromChar ch) + str
+
+let padRight (n:int) (ch:char) (str:string) =
+    str + repeat (n - length str) (fromChar ch)
+
+let trim (str:string) =
+    str.Trim()
+
+let trimLeft (str:string) =
+    str.TrimStart()
+
+let trimRight (str:string) =
+    str.TrimEnd()
+
+let map func str =
+    String.map func str
+
+let filter isGood str =
+    String.filter isGood str
+
+// let foldl func state str =
+
+
+// let foldr func state str =
+
+// let any isGood str =
+
+
+// let all isGood str =
