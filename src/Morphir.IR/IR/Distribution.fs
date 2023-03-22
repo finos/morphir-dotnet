d74c76768d3e46bcf91d79395c240e63a968f581
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-03-22 02:37:57 -0400

    Properly define the Distribution type in Morphir.IR.Distribution

--- src/Morphir.IR/IR/Distribution.fs
+++ src/Morphir.IR/IR/Distribution.fs
@@ -1,5 +1,32 @@
-module Morphir.IR.Distribution
+module rec Morphir.IR.Distribution
 
 open Morphir.IR.Package
+open Morphir.IR.Type
+open Morphir.SDK.Dict
 
-type Distribution = Library of packageName: PackageName
+/// Type that represents a package distribution. Currently the only distribution type we provide is a `Library`.
+type Distribution =
+    | Library of
+        packageName: PackageName *
+        dependencies: Dict<PackageName, Package.Specification<unit>> *
+        definition: Package.Definition<unit, Type<unit>>
+
+
+/// Get the package name of the distribution.
+let lookupPackageName (distribution: Distribution) : PackageName =
+    match distribution with
+    | Library (packageName, _, _) -> packageName
+
+/// Add a package specification as a dependency of this library.
+let insertDependency
+    (dependencyPackageName: PackageName)
+    (dependencyPackageSpec: Package.Specification<unit>)
+    (distribution: Distribution)
+    : Distribution =
+    match distribution with
+    | Library (packageName, dependencies, definition) ->
+        Library(
+            packageName,
+            insert dependencyPackageName dependencyPackageSpec dependencies,
+            definition
+        )
