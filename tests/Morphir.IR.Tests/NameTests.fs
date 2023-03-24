2675191e4fc564758269bbffad9f457ec89bb506
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-03-24 18:22:35 -0400

    Add Codec for Path

--- tests/Morphir.IR.Tests/NameTests.fs
+++ tests/Morphir.IR.Tests/NameTests.fs
@@ -31,7 +31,7 @@ let tests =
     let toTitleCaseTests =
         describe "toTitleCase" [
             let assert' inList outString =
-                testCase (sprintf "Title case %s" outString)
+                testCase $"Title case %s{outString}"
                 <| fun _ ->
                     Name.fromList inList
                     |> Name.toTitleCase
@@ -45,7 +45,7 @@ let tests =
     let toCamelCaseTests =
         describe "toCamelCase" [
             let assert' inList outString =
-                testCase (sprintf "Camel case %s" outString)
+                testCase $"Camel case %s{outString}"
                 <| fun _ ->
                     Name.fromList inList
                     |> Name.toCamelCase
@@ -59,7 +59,7 @@ let tests =
     let toSnakeCaseTests =
         describe "toSnakeCase" [
             let assert' inList outString =
-                testCase (sprintf "Snake case %s" outString)
+                testCase $"Snake case %s{outString}"
                 <| fun _ ->
                     Name.fromList inList
                     |> Name.toSnakeCase
@@ -73,7 +73,7 @@ let tests =
     let toHumanWordsTests =
         describe "toHumanWords" [
             let assert' inList outList =
-                testCase (sprintf "Human words %A" outList)
+                testCase $"Human words %A{outList}"
                 <| fun _ ->
                     Name.fromList inList
                     |> Name.toHumanWords
@@ -87,7 +87,7 @@ let tests =
     let encodeNameTests =
         describe "encodeName" [
             let assert' inList expectedText =
-                testCase (sprintf "where name is: %s" expectedText)
+                testCase $"where name is: %s{expectedText}"
                 <| fun _ ->
                     Name.fromList inList
                     |> Name.Codec.encodeName
