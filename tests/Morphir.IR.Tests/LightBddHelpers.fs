3dc0ab9e4bf145985759602766ca6e60098d12b4
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-04-03 20:24:23 -0400

    Codecs, formatting, and testing of Type IR (#64)
    * More Codec related BDD tests
    
    * Fix the warnings that are being emitted
    
    * Added a feature focused on formatting, also reorganized the testing code
    
    * Trying to use Thoth but hitting issue with recursive types
    
    * Added additional test cases for toString
    
    * Finish off implementation of Type.toString type
    
    * Finish off implementation of Type.toString type

--- tests/Morphir.IR.Tests/LightBddHelpers.fs
+++ tests/Morphir.IR.Tests/LightBddHelpers.fs
@@ -2,9 +2,11 @@
 module Morphir.IR.Tests.LightBddHelpers
 
 open System
+open System.Collections.Generic
 open System.Linq.Expressions
 open LightBDD.Framework
 open LightBDD.Framework.Scenarios
+open LightBDD.Framework.Parameters
 open Microsoft.FSharp.Core
 open Microsoft.FSharp.Quotations
 open ExpressionHelpers
@@ -13,12 +15,27 @@ type IBddRunner with
 
     member self.RunScenarioWithContext<'Context>
         ([<ParamArray>] steps: Expr<'Context -> unit> array)
-        =
+        : unit =
         let resolvedSteps: Expression<Action<'Context>>[] =
             steps
             |> Array.map Lambda.ofAction
 
         self.WithContext<'Context>().RunScenario(resolvedSteps)
+
+    member self.RunScenarioWithProvidedContext<'Context>
+        (
+            factory: unit -> 'Context,
+            [<ParamArray>] steps: Expr<'Context -> unit> array
+        ) =
+        let resolvedSteps: Expression<Action<'Context>>[] =
+            steps
+            |> Array.map Lambda.ofAction
+
+        self.WithContext<'Context>(Func<'Context>(factory)).RunScenario(resolvedSteps)
+
+    member self.WithProvidedContext<'TContext>(factory: unit -> 'TContext, ?takeOwnership: bool) =
+        let takeOwnership = defaultArg takeOwnership true
+        self.WithContext<'TContext>(Func<'TContext>(fun () -> factory ()), takeOwnership)
 
 type IBddRunner<'TContext> with
 
@@ -28,3 +45,10 @@ type IBddRunner<'TContext> with
             |> Array.map Lambda.ofAction
 
         self.RunScenario(resolvedSteps)
+
+let verifiableDataTableFromMap (map: Map<'k, 'v>) =
+    let dict = map :> IReadOnlyDictionary<'k, 'v>
+    dict.ToVerifiableDataTable()
+
+let verifiableDataTableFromList (list: #IReadOnlyList<'a>) = list.ToVerifiableDataTable()
+let toVerifiableDataTable (list: #IReadOnlyList<'a>) = list.ToVerifiableDataTable()
