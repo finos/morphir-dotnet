9a0cfa091abe795f1211f077f4ad154356730c77
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-04-07 14:17:44 -0400

    Finish up Codec for module

--- src/Morphir.IR/IR/Codecs/Module.fs
+++ src/Morphir.IR/IR/Codecs/Module.fs
@@ -51,3 +51,53 @@ let decodeSpecification decodeTypeAttributes:Decode.Decoder<Specification<'ta>> 
             Decode.field "doc" (Decode.maybe Decode.string)
             Decode.succeed Maybe.Nothing
         ])
+
+let encodeDefinition encodeTypeAttributes encodeValueAttributes def =
+    Encode.object [
+        "types",
+        def.Types
+        |> Dict.toList
+        |> Encode.list (fun (name, typeDef) ->
+            Encode.list id [
+                Name.encoder name
+                typeDef |> (AccessControlled.encoder (Documented.encoder (Type.encodeDefinition encodeTypeAttributes)))
+            ]
+        )
+        "values",
+        def.Values
+        |> Dict.toList
+        |> Encode.list (fun (name, valueDef) ->
+            Encode.list id [
+                Name.encoder name
+                valueDef |> (AccessControlled.encoder (Documented.encoder (Value.encodeDefinition encodeTypeAttributes encodeValueAttributes)))
+            ]
+        )
+        "def",
+        def.Doc |> Maybe.map Encode.string |> Maybe.withDefault Encode.nil
+    ]
+
+let decodeDefinition (decodeTypeAttributes:Decode.Decoder<'ta>) (decodeValueAttributes:Decode.Decoder<'va>) :Decode.Decoder<Definition<'ta,'va>> =
+    Decode.map3 definition
+        (Decode.field "types"
+            (Decode.map Dict.fromList
+                (Decode.list (Decode.map2 Tuple.pair
+                    (Decode.field "name" Name.decoder)
+                    (Decode.field "def" (AccessControlled.decoder (Documented.decoder (Type.decodeDefinition decodeTypeAttributes))))
+                ))
+            )
+         )
+        (Decode.field "values"
+            (Decode.map Dict.fromList
+                (Decode.list
+                    (Decode.map2 Tuple.pair
+                        (Decode.index 0 Name.decoder)
+                        (Decode.index 1 (AccessControlled.decoder (Documented.decoder (Value.decodeDefinition decodeTypeAttributes decodeValueAttributes))))
+                    )
+                )
+            )
+        )
+        (Decode.oneOf [
+            Decode.field "def" (Decode.maybe Decode.string)
+            Decode.succeed Maybe.Nothing
+        ])
+
