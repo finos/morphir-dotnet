a7ff00076f08f68a849ca7a0670a581808433f49
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-04-14 10:09:07 -0400

    Disable FSharpAnalyzers and only trigger release when we do a manual dispatch

--- build/build.fs
+++ build/build.fs
@@ -763,7 +763,7 @@ let initTargets () =
     "DotnetRestore"
         ==> "CheckFormatCode"
         ==> "DotnetBuild"
-        ==> "FSharpAnalyzers"
+        // ==> "FSharpAnalyzers"
         ==> "DotnetTest"
         =?> ("GenerateCoverageReport", not disableCodeCoverage)
         ==> "DotnetPack"
