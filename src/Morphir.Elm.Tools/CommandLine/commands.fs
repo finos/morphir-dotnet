d94f17edb60c1d74944ebb03d0ea20517cd9bf49
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-04-18 20:34:33 -0400

    Enable publishing Morphir as a .NET Global Tool and switched out CLI Stub (#123)
    * Start adding stubs for implementing CLI based on System.CommandLine and FSharp.SystemCommandLine
    
    * Stub Morphir CLI for use as a .NET tool

---
+++ src/Morphir.Elm.Tools/CommandLine/commands.fs
@@ -0,0 +1,44 @@
+﻿namespace Morphir.Elm.Tools.CommandLine
+
+open System.CommandLine.Help
+open System.CommandLine.Invocation
+open FSharp.SystemCommandLine
+
+[<RequireQualifiedAccess>]
+module ElmCommands =
+
+    let makeCommand =
+        let handler () = ()
+
+        command "make" {
+            description "Compile Elm source code"
+            setHandler handler
+        }
+
+    let genCommand =
+        let handler () = ()
+
+        command "gen" {
+            description "Generate Morphir code from Elm source code"
+            setHandler handler
+        }
+
+    let elmCommand =
+        let handler (ctx: InvocationContext) =
+            let cmd =
+                ctx.Parser.Configuration.RootCommand.Subcommands
+                |> Seq.find (fun c -> c.Name = "elm")
+
+            let hc = HelpContext(ctx.HelpBuilder, cmd, System.Console.Out)
+            ctx.HelpBuilder.Write(hc)
+
+
+        let ctx = Input.Context()
+
+        command "elm" {
+            description "Elm tooling for Morphir"
+
+            inputs ctx
+            addCommands [ makeCommand; genCommand ]
+            setHandler handler
+        }
