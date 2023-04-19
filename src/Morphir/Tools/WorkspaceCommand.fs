99a68f79ab1e7f1b9b3f37fc377114155f4c7e50
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-04-19 19:48:06 -0400

    Introduce dotnet-morphir (#126)

---
+++ src/Morphir/Tools/WorkspaceCommand.fs
@@ -0,0 +1,19 @@
+namespace Morphir.Tools
+
+open FSharp.SystemCommandLine
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+
+type WorkspaceCommand =
+    static member Handler(logger: ILogger<WorkspaceCommand>) =
+        fun () -> printfn "TODO: Implement workspace command"
+
+    static member Create(host: IHost) =
+        let logger = host.Services.GetService<ILogger<WorkspaceCommand>>()
+        let handler = WorkspaceCommand.Handler(logger)
+
+        command "workspace" {
+            description "Work with and manage a Morphir workspace"
+            setHandler handler
+        }
---
+++ src/dotnet-morphir/Program.fs
@@ -0,0 +1,6 @@
+﻿module Morphir.Tool.DotnetMorphir
+
+open Morphir.Tools
+open Morphir.Tools.CommandLine
+
+let main (args: string[]) = CommandLine.run args
