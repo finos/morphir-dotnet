f63fd1f71df28e3d3142f659b10c4e308d9bfd2b
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-04-20 18:44:35 -0400

    Myriad integration

---
+++ src/Morphir.Myriad.Plugins/MorphirIRGenerator.fs
@@ -0,0 +1,20 @@
+﻿namespace Morphir.Myriad.Plugins
+
+open Fantomas.Core
+open Myriad.Core
+open Fabulous.AST
+open type Fabulous.AST.Ast
+[<MyriadGenerator("MorphirIR")>]
+type MorphirIRGenerator() =
+    interface IMyriadGenerator with
+        member this.Generate(ctx) =
+            printfn $"Running MorphirIRGenerator for {ctx.InputFilename}"
+            let source =
+                    Module("Some.Foo") {
+                        Value("x", "43")
+                    }
+            let oak = Tree.compile source
+            let code = CodeFormatter.FormatOakAsync oak |> Async.RunSynchronously
+            Output.Source code
+
+        member this.ValidInputExtensions = seq { ".fs" }
