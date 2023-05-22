f715082e0007bb64c657916a0b6b0932f6bfe833
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-05-22 19:05:10 -0400

    Add ProjectSettings and an MSBuild Task to write them

---
+++ src/Morphir.Core/ProjectModel/ProjectSettings.fs
@@ -0,0 +1,30 @@
+namespace Morphir.ProjectModel
+
+open Thoth.Json.Net
+
+type ProjectSettings = {
+    PackageName: string
+    SourceDirectory: string
+    ExposedModules: string list
+}
+
+module ProjectSettings =
+    [<CompiledName("Decode")>]
+    let decode json =
+        Decode.Auto.fromString<ProjectSettings> json
+
+    [<CompiledName("Encode")>]
+    let encode (value: ProjectSettings) = Encode.Auto.toString value
+
+    let createOrUpdate newInstance (original: ProjectSettings option) : ProjectSettings =
+        match original with
+        | Some original -> {
+            PackageName = newInstance.PackageName
+            SourceDirectory = newInstance.SourceDirectory
+            ExposedModules = newInstance.ExposedModules
+          }
+        | None -> newInstance
+
+type ProjectSettings with
+    static member CreateOrUpdate(newInstance, original) =
+        ProjectSettings.createOrUpdate newInstance original
