f20b05c8e65cf86b919cba9b16408ea16f141437
DamianReeves <957246+DamianReeves@users.noreply.github.com>
2023-05-23 02:36:32 +0000

    Bump version to 0.1.0-Alpha-01
    ## [0.1.0-Alpha-01] - 2023-05-23
    
    ### Added
    - Targeted NetStandard 2.0 and .NET 6
    - Added Morphir.MSBuild

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,10 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
+## [0.1.0-Alpha-01] - 2023-05-23
 
 ### Added
-- Targeted NetStandard 2.0 and .NET 6 
+- Targeted NetStandard 2.0 and .NET 6
 - Added Morphir.MSBuild
 
 ## [0.0.1-alpha-008] - 2023-04-20
@@ -57,7 +57,8 @@ We are still working on the CLI tool and the Morphir SDK and the project is stil
 - Morphir IR ported from [morphir-elm](https://github.com/finos/morphir-elm)
 - Morphir SDK (incomplete)
 - Added the start of a CLI tool that is not at all ready for use.
-[Unreleased]: https://github.com/finos/morphir-dotnet/compare/v0.0.1-alpha-008...HEAD
+[Unreleased]: https://github.com/finos/morphir-dotnet/compare/v0.1.0-Alpha-01...HEAD
+[0.1.0-Alpha-01]: https://github.com/finos/morphir-dotnet/compare/v0.0.1-alpha-008...v0.1.0-Alpha-01
 [0.0.1-alpha-008]: https://github.com/finos/morphir-dotnet/releases/tag/v0.0.1-alpha-008
 [0.0.1-alpha-007]: https://github.com/finos/morphir-dotnet/releases/tag/v0.0.1-alpha-007
 [0.0.1-alpha-006]: https://github.com/finos/morphir-dotnet/releases/tag/v0.0.1-alpha-006
--- src/Morphir/AssemblyInfo.fs
+++ src/Morphir/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Morphir")>]
 [<assembly: AssemblyProductAttribute("Morphir")>]
-[<assembly: AssemblyVersionAttribute("0.0.1")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2023-04-20T00:00:00.0000000+00:00")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.0.1")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseChannel","alpha")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","8ed4ca356ac30d12289e8c413c34d734c48a4813")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2023-05-23T00:00:00.0000000+00:00")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseChannel","Alpha")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","73eab6e38259143389e5ec1421422fae74d130a8")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Morphir"
     let [<Literal>] AssemblyProduct = "Morphir"
-    let [<Literal>] AssemblyVersion = "0.0.1"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2023-04-20T00:00:00.0000000+00:00"
-    let [<Literal>] AssemblyFileVersion = "0.0.1"
-    let [<Literal>] AssemblyInformationalVersion = "0.0.1"
-    let [<Literal>] AssemblyMetadata_ReleaseChannel = "alpha"
-    let [<Literal>] AssemblyMetadata_GitHash = "8ed4ca356ac30d12289e8c413c34d734c48a4813"
+    let [<Literal>] AssemblyVersion = "0.1.0"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2023-05-23T00:00:00.0000000+00:00"
+    let [<Literal>] AssemblyFileVersion = "0.1.0"
+    let [<Literal>] AssemblyInformationalVersion = "0.1.0"
+    let [<Literal>] AssemblyMetadata_ReleaseChannel = "Alpha"
+    let [<Literal>] AssemblyMetadata_GitHash = "73eab6e38259143389e5ec1421422fae74d130a8"
