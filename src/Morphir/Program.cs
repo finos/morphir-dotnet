07b01775cacc1edaeea8f83395942b2b1f7d5d8d
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2025-07-07 23:45:32 -0500

    Setup of basic AOT compiled CLI

---
+++ src/Morphir/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.CommandLine;
+
+namespace Morphir;
+
+internal static class Program
+{
+    private static int Main(string[] args)
+    {
+        var rootCommand = new RootCommand("Morphir command line");
+        Command infoCommand = new("info", "Get information about the workspace/project");
+        rootCommand.Subcommands.Add(infoCommand);
+
+        infoCommand.SetAction(parseResult => { Console.WriteLine("Morphir command line info:"); });
+        return rootCommand.Parse(args).Invoke();
+    }
+}
