2edd4fe9e39dedb029ed816624eb5715a8ce8f11
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2025-07-08 00:28:50 -0500

    Adding additional run subcommand

--- src/Morphir/Program.cs
+++ src/Morphir/Program.cs
@@ -6,11 +6,31 @@ internal static class Program
 {
     private static int Main(string[] args)
     {
+        Option<FileInfo> currentDirectoryOption = new("-C")
+        {
+            Description = "Override the current directory",
+            Required = false
+        };
         var rootCommand = new RootCommand("Morphir command line");
         Command infoCommand = new("info", "Get information about the workspace/project");
         rootCommand.Subcommands.Add(infoCommand);
+        Command runCommand = new("run", "Run the Morphir compiler");
+        runCommand.Options.Add(currentDirectoryOption);
+        rootCommand.Subcommands.Add(runCommand);
 
         infoCommand.SetAction(parseResult => { Console.WriteLine("Morphir command line info:"); });
+        runCommand.SetAction(parseResult =>
+        {
+            var currentDirOverride = parseResult.GetValue(currentDirectoryOption);
+            if (currentDirOverride != null)
+            {
+                Console.WriteLine($"Current directory override: {currentDirOverride.FullName}");
+                Console.WriteLine($"Current directory override exists: {currentDirOverride.Exists}");
+                
+            }
+            Console.WriteLine("Morphir command line run:"); 
+            
+        });
         return rootCommand.Parse(args).Invoke();
     }
 }
