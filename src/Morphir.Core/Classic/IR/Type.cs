52580541a8a98c01c747919431fbca825b2a2ccb
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2025-10-14 23:22:39 -0500

    Refactor Type structure: Update Type.cs with new records and remove Symbol.cs

--- src/Morphir.Core/Classic/IR/Type.cs
+++ src/Morphir.Core/Classic/IR/Type.cs
@@ -1,6 +1,6 @@
 using Dunet;
 using Morphir.IR;
-using Type = Morphir.IR.Type;
+//using Type = Morphir.IR.Type;
 
 namespace Morphir.Classic.IR;
 
@@ -10,8 +10,23 @@ public abstract partial record Type<TAttrib>
     public required TAttrib Attributes { get; init; }
 
     public partial record Variable(Name Name);
-    public partial record Tuple(Seq<Type<TAttrib>> Items);
+    public partial record Reference(FqName TypeName, Seq<Type<TAttrib>> TypeParameters);
+    public partial record Tuple(Seq<Type<TAttrib>> ElementTypes);
+    public partial record Record(Seq<Type.Field<TAttrib>> FieldTypes);
+    public partial record ExtensibleRecord(Name VariableName, Seq<Type.Field<TAttrib>> FieldTypes);
     public partial record Function(Type<TAttrib> ParameterType, Type<Attribute> ReturnType);
     public partial record Unit;
 
+}
+
+public static class Type
+{
+    public record Field<TAttribute>(Name Name, Type<TAttribute> Type)
+        : Morphir.IR.Type.Field<Type<TAttribute>>(Name, Type)
+    {
+        
+    }
+    
+    public static Type<TAttributes> Variable<TAttributes>(TAttributes attributes, Name name) =>
+        new Type<TAttributes>.Variable(name){ Attributes = attributes };
 }
--- src/Morphir.Core/IR/Type.cs
+++ src/Morphir.Core/IR/Type.cs
@@ -4,4 +4,12 @@ public abstract record Type
 {
     public required Document Metadata { get; set; }
     public sealed record Variable(Name Name) : Type;
+
+    public sealed record Unit() : Type;
+
+    public record Field<T>(Name Name, T Type)
+    {
+        public Field<TResult> Map<TResult>(Func<T, TResult> mapper) =>
+            new(Name, mapper(Type));
+    }
 }
